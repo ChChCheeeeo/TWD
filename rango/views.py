5f82d4c4d6ba8da1388101abc6e312e5ab97ad88
ChChCheeeeo <cheo.roman+github@gmail.com>
2015-03-11 12:35:08 -0700

    update index view and template to display top five pages

--- rango/views.py
+++ rango/views.py
@@ -37,8 +37,12 @@ def category(request, category_name_slug):
 
 def index(request):
     category_list = Category.objects.order_by('-likes')[:5]
+    page_list = Page.objects.order_by(
+        '-views')[:5]
+
     context_dict = {
-        'categories': category_list
+        'categories': category_list,
+        'pages' : page_list,
     }
 
     return render(request, 'rango/index.html', context_dict)
