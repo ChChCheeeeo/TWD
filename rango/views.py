5e0cb6e43d501f665c5e8fc1a9075dc9ab089e69
ChChCheeeeo <cheo.roman+github@gmail.com>
2015-03-11 11:48:08 -0700

    fixed about/index views

--- rango/views.py
+++ rango/views.py
@@ -2,19 +2,23 @@ from django.http import HttpResponse
 from django.shortcuts import render
 from rango.models import Category
 
+
 def about(request):
-    category_list = Category.objects.order_by('-likes')[:5]
+    context_dict = {
+    	'boldmessage': "You've reached the about page",
+    }
+
+    return render(
+        request,
+        'rango/about.html',
+        context_dict
+    )	
+
+
+def index(request):
+	category_list = Category.objects.order_by('-likes')[:5]
     context_dict = {
         'categories': category_list
     }
 
     return render(request, 'rango/index.html', context_dict)
-
-
-
-def index(request):
-	context_dict = {
-		'boldmessage': "I am bold font from the context"
-	}
-	return render(request, 'rango/index.html', context_dict)
-
