c5dac92fb4ea91b2f17b1b9520e37065aa9d1807
ChChCheeeeo <cheo.roman+github@gmail.com>
2015-03-21 10:28:08 -0700

    added some jquery to about.html

--- static/js/rango-jquery.js
+++ static/js/rango-jquery.js
@@ -11,10 +11,58 @@ $(document).ready(function() {
 	  standard, to wait until the document has
 	  been finished loading before running
 	   the JQuery functions.
+
+	# used to find id 
+	. used to find classes
+
 */ 
 
 	$("#about-btn").click( function(event){
 		alert("You clicked the button using Jqury");
 
 	});
-});
+
+
+	$("p").hover(
+		/*
+			Over "p" elements, should change from
+			blue to red. Chaning "this" to "p" 
+			breaks the color change.
+			the hover function to assign an event
+			handler to the on hover event, and
+			then used the css function to change 
+			the color of the element. 
+		*/
+		
+		function() {
+            $(this).css('color', 'red');
+    	},
+   		
+   		function() {
+            $(this).css('color', 'blue');
+    	}
+    );//end hover
+
+    $("#about-btn").addClass('btn btn-primary')
+    $("#about-btn").click( function(event) {
+    	/*
+    		It is also possible to access the 
+    		html of a particular element.
+    		On click of the element with id 
+    		#about-btn, we first get the html 
+    		inside the element with id msg and 
+    		appeand “o” to it. Then we change 
+    		the html inside the element by 
+    		calling the html function again, 
+    		but this time passing through string 
+    		msgstr to replace the html inside 
+    		that element.
+    	*/
+		
+		msgstr = $("#msg").html()
+		msgstr = msgstr + "o"
+
+		$("#msg").html(msgstr)
+	 }); // end click
+
+}); // end document ready
